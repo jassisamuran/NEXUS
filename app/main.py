bf925a562f139cbc70d47bb739c37179b15a06e9
jassisamuran <samuran3132@gmail.com>
2026-04-01 00:18:31 +0530

    added routes for  task and auth

--- app/main.py
+++ app/main.py
@@ -8,20 +8,30 @@ from fastapi.staticfiles import StaticFiles
 
 from app.config import settings
 from app.database.connection import create_tables
-
+from app.auth.router import router as auth_router
+from app.tasks.router import router as tasks_router
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # startup
     os.makedirs(settings.REPOS_DIR, exist_ok=True)
-    create_tables
+    await create_tables()
     print(f"{settings.APP_NAME} v{settings.APP_VERSION} started")
     yield
 
     print("Shutting down")
 
 
+
+
+
 app = FastAPI(title=settings.APP_NAME, version=settings.APP_VERSION, lifespan=lifespan)
+app.include_router(auth_router)
+app.include_router(tasks_router)
+
+@app.get("/health")
+async def health():
+    return {"status": "healthy", "version": settings.APP_VERSION}
 
 app.add_middleware(
     CORSMiddleware,
@@ -29,6 +39,8 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
 
 # app.mount("/static", StaticFiles(directory="frontend/assets"), name="static")
 
